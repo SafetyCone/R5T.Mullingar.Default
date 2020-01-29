98be6ef9e529b1b784a957a7e5017b735423848f
MinexAutomation <minex.automation@gmail.com>
2020-01-28 23:41:49 -0800

    Added IHelloWorldMessageProvider service implementation HelloWorldMessageProvider.

---
+++ source/R5T.Mullingar.Default/Code/Services/Implementations/HelloWorldMessageProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Mullingar.Default
+{
+    public class HelloWorldMessageProvider : IHelloWorldMessageProvider
+    {
+        public const string HelloWorldMessage = "Hello world!";
+
+
+        public string GetHelloWorldMessage()
+        {
+            return HelloWorldMessageProvider.HelloWorldMessage;
+        }
+    }
+}
